9cba93702c0365bb096a7bd3e7c719888ae9857b
Huquan666 <r94z82tz88@privaterelay.appleid.com>
2026-04-30 23:48:58 +0800

    v2.0: 升级程序，增加键盘输入功能

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Homework-149
 ## ex01 - HelloWorld程序
+### v1.0
 基础版HelloWorld程序，实现控制台输出"Hello, World!"。
+
+### v2.0
+升级版本，增加键盘输入功能，可读取用户输入并输出内容。
--- ex01/hello.c
+++ ex01/hello.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    char input[100];
+    printf("请输入一段内容：");
+    scanf("%s", input);
+    printf("你输入的内容是：%s\n", input);
     return 0;
 }
